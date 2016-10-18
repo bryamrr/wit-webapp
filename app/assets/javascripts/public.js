199a41a210a8a82bb86e40264e8dde23c35f5046
Bryam Rodriguez <brodriguezaybar@gmail.com>
2016-10-18 16:38:05 -0500

    Add experts section

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -11,26 +11,20 @@ $(document).on('turbolinks:load', function () {
   var owl = $("#owl-demo");
 
     owl.owlCarousel({
-        items : 10, //10 items above 1000px browser width
-        itemsDesktop : [1000,5], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,3], // betweem 900px and 601px
-        itemsTablet: [600,2], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+      items : 4,
+      itemsDesktop : [1000,3],
+      itemsDesktopSmall : [900,2],
+      itemsTablet: [600,1],
+      itemsMobile : false
     });
 
     // Custom Navigation Events
     $(".next").click(function(){
       owl.trigger('owl.next');
-    })
+    });
     $(".prev").click(function(){
       owl.trigger('owl.prev');
-    })
-    $(".play").click(function(){
-      owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-    })
-    $(".stop").click(function(){
-      owl.trigger('owl.stop');
-    })
+    });
 });
 
 $(window).scroll(function() {
