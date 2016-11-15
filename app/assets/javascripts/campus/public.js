6d19646f7cdce39d197cb2e25f77f3db94d2fa8e
Bryam Rodriguez <brodriguezaybar@gmail.com>
2016-11-15 11:08:58 -0500

    Add top menu items

--- app/assets/javascripts/campus/public.js
+++ app/assets/javascripts/campus/public.js
@@ -1,3 +1,11 @@
 $(document).on('turbolinks:load', function () {
-
+  $("#shopping-cart").click(function () {
+    console.log("shopping-cart");
+  });
+  $("#team-list").click(function () {
+    console.log("team-list");
+  });
+  $("#settings").click(function () {
+    console.log("settings");
+  });
 });
