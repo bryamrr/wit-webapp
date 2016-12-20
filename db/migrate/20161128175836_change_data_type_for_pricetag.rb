9513f44378ee7aa380621d8fdeefe4c61285fdee
Bryam Rodriguez <brodriguezaybar@gmail.com>
2016-12-20 17:44:33 -0500

    wip

--- db/migrate/20161128175836_change_data_type_for_pricetag.rb
+++ db/migrate/20161128175836_change_data_type_for_pricetag.rb
@@ -1,7 +1,7 @@
 class ChangeDataTypeForPricetag < ActiveRecord::Migration[5.0]
   def self.up
       change_table :courses do |t|
-        t.change :pricetag, :decimal
+        t.change :pricetag, 'decimal USING CAST(pricetag AS numeric)'
       end
     end
     def self.down
